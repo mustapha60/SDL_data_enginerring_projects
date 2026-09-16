728752f67de325047fc654f30a87305962975513
mustapha Aliu <68871343+mustapha60@users.noreply.github.com>
2026-09-16 10:47:54 +0100

    Update 1.2 top skills.sql

--- 1.11 lesson/1.EDA/1.2 top skills.sql
+++ 1.11 lesson/1.EDA/1.2 top skills.sql
@@ -6,4 +6,4 @@ inner join skills_job_dim as sjd on jpf.job_id=sjd.job_id
 inner join skills_dim as sd on sjd.skill_id=sd.skill_id
 group by sd.skills
 order by median_salary DESC
-limit 25;
+limit 100;
